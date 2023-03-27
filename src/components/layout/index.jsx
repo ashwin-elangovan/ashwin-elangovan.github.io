e5822fa99645186ad21a8cbe29b908b5d35b6489
Ashwin Elangovan <ashelangovan@gmail.com>
2023-03-26 20:27:01 -0700

    Added statuspage links

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
+import {
+  SiStatuspage
+} from 'react-icons/si';
+
 
 const classes = {
   wrapper: 'p-8 relative max-w-screen-xl xs:p-24',
   outerWrapper: 'relative dark:bg-gray-800',
   svg: 'hidden fixed transform right-0 top-5 z-0 xl:block',
+  list: 'mt-6 uppercase tracking-wider',
+  item: 'inline list-none pr-4',
+  link: 'inline-flex items-center py-2 font-semibold text-xs text-gray-600 hover:text-black dark:text-gray-100 dark:hover:text-blue-400',
+  linkName: 'ml-1'
 };
 
 const Layout = ({ children }) => {
   return (
     <div className={classes.outerWrapper}>
+      <div>
+        <ul className={classes.list} style={{ position: "absolute", top: -10, right: 10 }}>
+          <li className={classes.item}>
+            <a
+              className={classes.link}
+              href={`https://ashwin.statuspage.io/`}
+              rel="noreferrer"
+              target="_blank"
+            >
+              <SiStatuspage /> <span className={classes.linkName}>STATUS PAGE</span>
+            </a>
+          </li>
+        </ul>
+      </div>
       <svg
         className={classes.svg}
         width="404"
