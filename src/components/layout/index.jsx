db68e237c870db26dc62fbd6fa27a9ff1a44f784
Ashwin Elangovan <ashelangovan@gmail.com>
2023-04-01 09:44:44 -0700

    Minor fixes

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -7,7 +7,7 @@ import {
 const classes = {
   wrapper: 'p-8 relative max-w-screen-xl xs:p-24',
   outerWrapper: 'relative dark:bg-gray-800',
-  svg: 'hidden fixed transform right-0 top-5 z-0 xl:block',
+  svg: 'hidden fixed transform right-0 z-0 xl:block',
   list: 'mt-6 uppercase tracking-wider',
   item: 'inline list-none pr-4',
   link: 'inline-flex items-center py-2 font-semibold text-xs text-gray-600 hover:text-black dark:text-gray-100 dark:hover:text-blue-400',
@@ -42,6 +42,7 @@ const Layout = ({ children }) => {
       )}
       <svg
         className={classes.svg}
+        style={{ top: '10%' }}
         width="404"
         height={svgHeight}
         fill="none"
